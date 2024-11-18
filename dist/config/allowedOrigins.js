6cac6fd58e59aff439867ff4a73b2e3b5ed947a2
vernomad <veron988@gmail.com>
2024-11-19 08:22:49 +1300

    update commit

--- dist/config/allowedOrigins.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const allowedOrigins = [
-    'http://localhost:5173',
-    'http://localhost:3500',
-    'http://185.valab.cloud',
-    'https://185.valab.cloud',
-];
-exports.default = allowedOrigins;
--- dist/config/role_list.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ROLES_LIST = {
-    Admin: true || false,
-    Editor: true || false
-};
-exports.default = ROLES_LIST;
