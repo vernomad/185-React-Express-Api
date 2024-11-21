f81bbaf5f0c02c3ae189886eae50059200c37a4c
vernomad <veron988@gmail.com>
2024-11-22 08:27:38 +1300

    process.env client

--- clients/src/components/LogoutButton.tsx
+++ clients/src/components/LogoutButton.tsx
@@ -2,16 +2,17 @@ import { useState, useTransition } from "react";
 import { SlLogout } from "react-icons/sl";
 
 const Logout = () => {
-    const [error, setError] = useState<null | string>(null);
-    const [isPending, startTransition] = useTransition();
+  const [error, setError] = useState<null | string>(null);
+  const [isPending, startTransition] = useTransition();
   const [isFetching, setIsFetching] = useState(false);
   const isMutating = isFetching || isPending;
+  const baseUrl = process.env.VITE_BASE_URL || 'https://185.valab.cloud';
 
     const handleLogout = async () => {
         try {
             setIsFetching(true);
             setError("")
-            const res = await fetch("http://localhost:3500/api/auth/logout", {
+            const res = await fetch(`${baseUrl}/api/auth/logout`, {
                 method: "DELETE",
                 headers: {
                     'content-type': 'application/json',
