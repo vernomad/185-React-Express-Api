d5f142a4d2a029224e96d7eb87718db8c3b1b36b
vernomad <veron988@gmail.com>
2025-11-25 10:26:08 +1300

    css vh updates

--- clients/src/main.tsx
+++ clients/src/main.tsx
@@ -5,19 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
 
-// --- Fix mobile viewport height ---
-function setViewportHeight() {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-
-// run immediately
-setViewportHeight();
-
-// update if viewport size significantly changes
-window.addEventListener('resize', setViewportHeight);
-
-// --- END FIX ---
 
 createRoot(document.getElementById('root')!).render(
    <React.StrictMode>
